ae4eab4e2b14dac1b18d6700480c459501b180c7
Ian Spence <ian@ecnepsnai.com>
2026-04-16 21:58:13 -0700

    Reorganize extra views

---
+++ dns-inspector/Views/Extra Views/LabeledTextField.swift
@@ -0,0 +1,40 @@
+// DNS Inspector
+// Copyright (C) Ian Spence and other DNS Inspector Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import SwiftUI
+
+struct LabeledTextField<Content: View>: View {
+    let label: String
+    let content: () -> Content
+
+    init(label: String, @ViewBuilder content: @escaping () -> Content) {
+        self.label = label
+        self.content = content
+    }
+
+    var body: some View {
+        if #available(iOS 16, *) {
+            LabeledContent(self.label) {
+                self.content()
+            }
+        } else {
+            HStack {
+                Text(self.label)
+                self.content()
+            }
+        }
+    }
+}
